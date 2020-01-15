a3f8befcd241581ec6bc56f0d24bd260c700d70c
Emery Tate <tateejt@gmail.com>
2020-01-15 21:04:46 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,17 +1,5 @@
 class AuthorsController < ApplicationController
   
-  def posts_index
-    @author = Author.find(params[:id])
-    @posts = @author.posts 
-    render template: 'posts/index'
-  end
-  
-  def post 
-    @author = Author.find(params[:id])
-    @post = Post.find(params[:post_id])
-    render template: 'posts/show'
-  end
-
   def show
     @author = Author.find(params[:id])
   end
